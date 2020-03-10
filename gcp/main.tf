c78f973bc4875ef40bf7a1ccecfee6764603db29
Gyuseok.Lee <hahohh@gmail.com>
2020-03-10 22:18:07 +0900

    change env value

--- gcp/main.tf
+++ gcp/main.tf
@@ -1,5 +1,13 @@
-resource "null_resource" "demo" {
-  provisioner "local-exec" {
-    command = "echo hello world"
-  }
+provider "google" {
+  version = "3.5.0"
+
+  credentials = var.credentials
+
+  project = var.project
+  region  = var.region
+  zone    = var.zone_a
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
 }
