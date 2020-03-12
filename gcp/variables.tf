0e39ee9a94b77d45efcd977897f703d30e8152f2
Gyuseok.Lee <hahohh@gmail.com>
2020-03-12 22:38:11 +0900

    add default gcp variables

--- gcp/variables.tf
+++ gcp/variables.tf
@@ -1,9 +1,15 @@
-variable "project" {}
-variable "region" {}
+variable "project" {
+    default = "terraform-test-263205"
+}
+variable "region" {    
+    default = "asia-northeast1"
+}
 variable "zone" {
     type    = list
     default = [
         "asia-northeast1-a",
     ]
 }
-variable "credentials" {}
+variable "credentials" {
+    default = file("terraform-test-gcp.json")
+}
